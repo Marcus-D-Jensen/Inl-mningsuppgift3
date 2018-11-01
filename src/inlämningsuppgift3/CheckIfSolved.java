4fcd664a91df55ceafa73b1ed840a19e84eeec9f
Marcus Damsgaard Jensen <marcus8209@gmail.com>
2018-11-01 23:19:20 +0100

    Merge FeatureBranchB
    Conflicts:
    	src/inlämningsuppgift3/myStyle.css

--- src/inlämningsuppgift3/CheckIfSolved.java
+++ src/inlämningsuppgift3/CheckIfSolved.java
@@ -1,25 +1,24 @@
 package inlämningsuppgift3;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import javafx.scene.control.Button;
 
 public class CheckIfSolved {
 
     private Button buttonEmpty = new Button("");
-    private ArrayList<Button> gkjnfld = new ArrayList();
+    private ArrayList<Button> tempButtons = new ArrayList();
     
     public boolean solved(ArrayList<Button> buttons, Button empty) {
         
-        gkjnfld.clear();
+        tempButtons.clear();
         
         for (Button button : buttons) {
-            gkjnfld.add(button);
+            tempButtons.add(button);
         }
         
-        gkjnfld.remove(empty);
+        tempButtons.remove(empty);
         int i = 1;
-        for (Button button : gkjnfld) {
+        for (Button button : tempButtons) {
             
             if (!button.getText().equals(String.valueOf(i))) {
                 return false; 
