eca14f13d794dd5443dd51970c206cc87bb7c4b6
Marcus Damsgaard Jensen <marcus8209@gmail.com>
2018-11-01 15:10:51 +0100

    La till knapparna i en ArrayList. Så att jag kan blanda dom. Gjorde även "empty"-knappen osynlig.

--- src/inlämningsuppgift3/Inlämningsuppgift3.java
+++ src/inlämningsuppgift3/Inlämningsuppgift3.java
@@ -1,16 +1,18 @@
 package inlämningsuppgift3;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Inlämningsuppgift3 extends Application{
-
-    Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15;
-    Label empty;
+    
+    Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, empty;
+    
+    ArrayList<Button> buttons = new ArrayList();
     
     public static void main(String[] args) {
         launch(args);
@@ -23,16 +25,32 @@ public class Inlämningsuppgift3 extends Application{
         b1 = new Button("1");   b2 = new Button("2");   b3 = new Button("3");   b4 = new Button("4");
         b5 = new Button("5");   b6 = new Button("6");   b7 = new Button("7");   b8 = new Button("8");
         b9 = new Button("9");   b10 = new Button("10"); b11 = new Button("11"); b12 = new Button("12");
-        b13 = new Button("13"); b14 = new Button("14"); b15 = new Button("15"); empty = new Label();
+        b13 = new Button("13"); b14 = new Button("14"); b15 = new Button("15"); empty = new Button();
         
-        GridPane layout = new GridPane();
+        empty.setId("emptySquare");
         
-        layout.add(b1,0,0);     layout.add(b2,1,0);     layout.add(b3, 2, 0);       layout.add(b4, 3, 0);
-        layout.add(b5,0,1);     layout.add(b6,1,1);     layout.add(b7, 2, 1);       layout.add(b8, 3, 1);
-        layout.add(b9,0,2);     layout.add(b10,1,2);    layout.add(b11, 2, 2);      layout.add(b12, 3, 2);
-        layout.add(b13,0,3);    layout.add(b14,1,3);    layout.add(b15, 2, 3);      layout.add(empty, 3, 3);
+        buttons.add(b1);    buttons.add(b2);    buttons.add(b3);    buttons.add(b4);
+        buttons.add(b5);    buttons.add(b6);    buttons.add(b7);    buttons.add(b8);
+        buttons.add(b9);    buttons.add(b10);   buttons.add(b11);   buttons.add(b12);
+        buttons.add(b13);   buttons.add(b14);   buttons.add(b15);   buttons.add(empty);
         
-        Scene scene = new Scene(layout);
+        GridPane gridLayout = new GridPane();
+        int row = 0;
+        int col = 0;
+        
+        Collections.shuffle(buttons);
+        
+        for (Button button : buttons) {
+            gridLayout.add(button, col, row);
+            if (col < 3) {
+                col++;
+            } else {
+                col = 0;
+                row++;
+            }
+        }
+        
+        Scene scene = new Scene(gridLayout);
         primaryStage.setScene(scene);
         
         scene.getStylesheets().add(Inlämningsuppgift3.class.getResource("myStyle.css").toExternalForm());
